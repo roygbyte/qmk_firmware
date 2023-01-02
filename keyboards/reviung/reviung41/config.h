c2b5ee661aa4bd061433a8176308fe8b863ed6e8
Scarlett <social@scarlettmcallister.com>
2023-01-02 14:27:30 -0400

    Add Reviung Files

--- keyboards/reviung/reviung41/config.h
+++ keyboards/reviung/reviung41/config.h
@@ -135,3 +135,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Bootmagic Lite key configuration */
 // #define BOOTMAGIC_LITE_ROW 0
 // #define BOOTMAGIC_LITE_COLUMN 0
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
