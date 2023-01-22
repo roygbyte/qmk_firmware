dcd9aaebfc88acd54c683ad22a7163be4e05a90a
Scarlett <social@scarlettmcallister.com>
2023-01-21 20:45:13 -0400

    Add crkbd files

---
+++ keyboards/crkbd/keymaps/roygbyte/config.h
@@ -0,0 +1,58 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+/* #define MASTER_RIGHT */
+// #define EE_HANDS
+
+#define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+
+#define NO_ACTION_ONESHOT
+// Because the Macro and Function features are incompatible with Link Time Optimization, disable those features in
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+
+#define SPLIT_WPM_ENABLE
+
+#ifdef RGB_MATRIX_ENABLE
+#   define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+#   define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
+#   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
+#    define RGB_MATRIX_HUE_STEP 8
+#    define RGB_MATRIX_SAT_STEP 8
+#    define RGB_MATRIX_VAL_STEP 8
+#    define RGB_MATRIX_SPD_STEP 10
+#    define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+/* #    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE */
+#    define ENABLE_RGB_MATRIX_SPLASH
+#    define ENABLE_RGB_MATRIX_SOLID_SPLASH
+#endif
+
+#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
---
+++ keyboards/crkbd/keymaps/roygbyte/rules.mk
@@ -0,0 +1,17 @@
+#EXTRAFLAGS += -flto # This enables Link Time Optimization, saving a significant amount of space.
+RGB_MATRIX_ENABLE = yes
+OLED_ENABLE     = yes
+SPACE_CADET_ENABLE = yes
+
+#OLED_DRIVER     = SSD1306
+WPM_ENABLE = yes
+
+# Size optimisations
+EXTRAKEY_ENABLE = no # media keys
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+LTO_ENABLE = yes
+GRAVE_ESCAPE_ENABLE = no
+MOUSEKEY_ENABLE = no
+RGBLIGHT_ENABLE = no # Enable WS2812 RGB underlight.
+# Size optimisations END
